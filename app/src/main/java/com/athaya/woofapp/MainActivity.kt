21f012b55d3b8942faae80a203e307fd65d53e71
Miftahul93 <miftahululum693@gmail.com>
2023-12-26 21:44:51 +0700

    Initial commit

---
+++ app/src/main/java/com/athaya/woofapp/MainActivity.kt
@@ -0,0 +1,109 @@
+package com.athaya.woofapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.athaya.woofapp.data.Dog
+import com.athaya.woofapp.data.dogs
+import com.athaya.woofapp.ui.theme.WoofAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            WoofAppTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    WoofApp()
+                }
+            }
+        }
+    }
+}
+
+/**
+ * Composable that displays an app bar and a list of dogs.
+ */
+@Composable
+fun WoofApp() {
+    LazyColumn {
+        items(dogs) {
+            DogItem(dog = it)
+        }
+    }
+}
+
+@Composable
+fun DogItem(
+    dog: Dog,
+    modifier: Modifier = Modifier
+) {
+    Row(
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(dimensionResource(R.dimen.padding_small))
+    ) {
+        DogIcon(dog.imageResourceId)
+        DogInformation(dog.name, dog.age)
+    }
+}
+
+@Composable
+fun DogIcon(
+    @DrawableRes dogIcon: Int,
+    modifier: Modifier = Modifier
+) {
+    Image(
+        modifier = modifier
+            .size(dimensionResource(R.dimen.image_size))
+            .padding(dimensionResource(id = R.dimen.padding_small)),
+        painter = painterResource(id = dogIcon),
+        contentDescription = null,
+    )
+}
+
+@Composable
+fun DogInformation(
+    @StringRes dogName: Int,
+    dogAge: Int,
+    modifier: Modifier = Modifier
+) {
+    Column(modifier = modifier) {
+        Text(
+            text = stringResource(id = dogName),
+            modifier = Modifier.padding(top = dimensionResource(id = R.dimen.padding_small))
+        )
+        Text(text = stringResource(id = R.string.years_old, dogAge))
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun WoofPreview() {
+    WoofAppTheme(darkTheme = false) {
+        WoofApp()
+    }
+}
