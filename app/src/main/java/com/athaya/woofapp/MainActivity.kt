7a352e874103e6c99667eed8401358f9b4691c4c
Miftahul93 <miftahululum693@gmail.com>
2023-12-28 23:30:31 +0700

    Menambahkan pengatur jarak icon ke baris item daftar

--- app/src/main/java/com/athaya/woofapp/MainActivity.kt
+++ app/src/main/java/com/athaya/woofapp/MainActivity.kt
@@ -8,6 +8,7 @@ import androidx.annotation.StringRes
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -26,6 +27,10 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -108,6 +113,9 @@ fun DogItem(
     dog: Dog,
     modifier: Modifier = Modifier
 ) {
+    var expanded by remember {
+        mutableStateOf(false)
+    }
     Card(
         modifier = modifier
     ) {
@@ -118,6 +126,12 @@ fun DogItem(
         ) {
             DogIcon(dog.imageResourceId)
             DogInformation(dog.name, dog.age)
+            Spacer(modifier = Modifier.weight(1f))
+            DogItemButton(
+                expanded = expanded,
+                onClick = {
+                }
+            )
         }
     }
 }
@@ -135,7 +149,8 @@ private fun DogItemButton(
         Icon(
             imageVector = Icons.Filled.ExpandMore,
             contentDescription = stringResource(id = R.string.expand_button_content_description),
-            tint = MaterialTheme.colorScheme.secondary)
+            tint = MaterialTheme.colorScheme.secondary
+        )
     }
 }
 
